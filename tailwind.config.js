07e419fb0bdae198f0564e520e0bf5b0dc72c380
Jan Panzenberger <jan@panzenberger.cz>
2024-06-12 13:08:04 +0200

    ramecek

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "inter": ["Inter", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
