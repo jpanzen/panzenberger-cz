3b87ea42be86a91eb25d7d676925dc0e0df68697
Jan Panzenberger <jan@panzenberger.cz>
2024-06-16 19:07:41 +0200

    projekty, skills, nabidka, formular (neinteraktivni zatim)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "inter": ["Inter", "sans-serif"],
+        "poppins": ["Poppins", "sans-serif"],
+        "nunito": ["Nunito", "sans-serif"]
+      },
+      colors: {
+        "dark": "#161616",
+        "green": "#096264",
+        "green100": "009598",
+        "gold": "#8C673B",
+        "gold100": "#DB9D54",
+        "white10": "rgba(255, 255, 255, 0.1)",
+        "white20": "rgba(255, 255, 255, 0.2)",
+        "gray": "#BCBCBC",
+      },
+      backgroundImage: {
+        "colors": "url('/assets/colors-czerwinski.jpg')"
       }
     },
   },
