0e25492c1e73d2876ee884b4099bf7c9468ac7a8
Jan Panzenberger <jan@panzenberger.cz>
2024-06-30 00:52:45 +0200

    rebuild2

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ const { DateTime } = require("luxon");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/output.css");
+  eleventyConfig.addPassthroughCopy("src/post-styles.css");
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_HUGE)
@@ -15,14 +16,11 @@ module.exports = function(eleventyConfig) {
 
   // Put robots.txt in root
   eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
-  
-  // Seznam webmaster
-  
     
-    return {
-      dir: {
-        input: "src",
-        output: "docs"
-      }
-    };
+  return {
+    dir: {
+      input: "src",
+      output: "docs"
+    }
   };
+};
